ab132a0f27d07799da10359f50172592e1e87ce0
Matt Wong <matt.wong.rpi@gmail.com>
2025-02-19 16:32:38 -0500

    setup snacks file explorer, tree sitter, and colorscheme

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  'catppuccin/nvim',
+  lazy = false,
+  name = 'catppuccin',
+  priority = 1000,
+  opts = {
+    flavour = 'macchiato',
+    transparent_background = true,
+    integrations = {
+      snacks = true,
+      treesitter = true
+    }
+  },
+  config = function(_, opts)
+    require('catppuccin').setup(opts)
+
+    vim.cmd 'colorscheme catppuccin'
+  end
+}
---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,9 @@
+return {
+  'folke/snacks.nvim',
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    explorer = { enabled = true },
+    picker = { enabled = true }
+  }
+}

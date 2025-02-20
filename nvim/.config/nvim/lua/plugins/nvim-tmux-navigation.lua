93ea11521a26b72d34bc41407019ff68b17f2a1d
Matt Wong <matt.wong.rpi@gmail.com>
2025-02-20 09:21:40 -0500

    setup tmux nav

---
+++ nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "alexghergh/nvim-tmux-navigation",
+    lazy = false,
+    opts = {
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      },
+    },
+  },
+}
